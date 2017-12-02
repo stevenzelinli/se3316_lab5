2a35292aa438fa799b39f332d771ec14c0ef034d
Steven Li <steven.zelin.li@gmail.com>
2017-12-02 11:04:58 -0500

    Created base components: nav-bar, profile, search, home

--- nasa-image-fetcher/src/app/app.module.ts
+++ nasa-image-fetcher/src/app/app.module.ts
@@ -3,14 +3,45 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { 
+    path: 'home',
+    component: HomeComponent
+  },
+  { 
+    path: 'profile',
+    component: ProfileComponent
+  },
+  { 
+    path: 'search',
+    component: SearchComponent
+  },
+  { 
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
