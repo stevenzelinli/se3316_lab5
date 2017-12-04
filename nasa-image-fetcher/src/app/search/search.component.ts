ec1402c91cd6f62a21c33e62d291bd8dce15efca
Steven Li <steven.zelin.li@gmail.com>
2017-12-04 16:17:34 -0500

    Created functionality for searching nasa image archives on the search page. Put placeholder button that will eventually allow the user to add to a collection

--- nasa-image-fetcher/src/app/search/search.component.ts
+++ nasa-image-fetcher/src/app/search/search.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
+    private imagesResponse: ImagesResponse;
+    private imageURLs: string[];
+    constructor(
+        private http: HttpClient
+    ) { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    search(what: string): void{
+        let params = new HttpParams().set("q", what).set("media_type", "image");
+        this.http.get<ImagesResponse>('https://images-api.nasa.gov/search', { params: params }).subscribe(
+            data => {
+                this.imagesResponse = data;
+                this.imageURLs = [];
+                this.imagesResponse.collection.items.forEach(image =>{
+                    this.imageURLs.push(image.links[0].href);
+                });
+            },
+            err => {
+                this.imagesResponse = null;
+                this.imageURLs = [];
+            }
+        );
+    }
 
+}   
+interface ImagesResponse{
+    collection: {
+        href: string;
+        items: {
+            data: {
+                center: string;
+                date_created: string;
+                description: string;
+                keywords: string[];
+                media_type: string[];
+                nasa_id: string;
+                title: string;
+            }[];
+            href: string;
+            links: {
+                href: string;
+                rel: string;
+                render: string;
+            }[];
+        }[];
+        links: {
+            href: string;
+            prompt: string;
+            rel: string;
+        }[];
+        metadata: {
+            total_hits: number;
+        };
+        version: string;
+    };
 }
